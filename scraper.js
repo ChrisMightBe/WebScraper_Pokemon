1bf2100da3b7ad4b115dd4d0f13181d40d4ae221
ChrisMightBe <cjaydunphy@gmail.com>
2025-09-05 21:48:06 +0100

    corrected url name, searched fir the dex table 	modified:   scraper.js

--- scraper.js
+++ scraper.js
@@ -1,11 +1,11 @@
 const cheerio = require('cheerio');
 
-(async() => {
-    const url =;
-    const response = await fetch(url);
+(async () => {
+    const url = 'https://serebii.net/pokemon/';  // Url of Serebii's pokemon page, the most bas epage with dex access
+    const response = await fetch(url);  // fetch returns a promise, so we await it
+    const html = await response.text(); // awaits .text() apparently, so the function will actually work?
+    const $ = cheerio.load(html);  // loads html into cheerio
 
-    const $ = cheerio.load(await response.text());
-    console.log($.html());
+    const $dextab = $('table.dextab');
+    console.log($dextab.html());
 })();
-
-// dextab
